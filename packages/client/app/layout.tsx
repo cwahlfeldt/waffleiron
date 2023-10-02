e5d7313d31d3685d3bc254c69bccaf092dd16b7d
cwahlfeldt <chriswahlfeldt@gmail.com>
2023-10-01 20:43:54 -0500

    yay

--- packages/client/app/layout.tsx
+++ packages/client/app/layout.tsx
@@ -8,6 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
       <body>{children}</body>
     </html>
   );
